3f6b355a61e1dd96c08f8c476117c8f7141fd9f9
Daniel Mensinger <d.mensinger@tup.com>
2023-04-11 13:02:08 +0200

    Multithreading locking error

---
+++ src/main/java/root/TestEntity.java
@@ -0,0 +1,40 @@
+package root;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
+@Entity
+public class TestEntity {
+
+    @Id
+    private long id;
+
+    @Version
+    @Column(columnDefinition = "NUMERIC(9) DEFAULT 0")
+    private long version;
+
+    private int counter;
+
+    protected TestEntity() {
+        // For JPA
+    }
+
+    public TestEntity(long id) {
+        this.id = id;
+        this.counter = 0;
+    }
+
+    public void increment() {
+        counter += 1;
+    }
+
+    public long id() {
+        return id;
+    }
+
+    public int counter() {
+        return counter;
+    }
+}
