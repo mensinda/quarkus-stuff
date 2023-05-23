95d707c7e66f389c3be69d8d9da25b7a2a21331d
Daniel Mensinger <d.mensinger@tup.com>
2023-05-23 08:48:01 +0200

    Port to eclipselink

--- src/test/java/test/CreateTest.java
+++ src/test/java/test/CreateTest.java
@@ -1,72 +1,105 @@
 package test;
 
-import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.LockModeType;
-import jakarta.transaction.Transactional;
+import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.Test;
 import root.TestEntity;
 import root.TestNoVersEntity;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@QuarkusTest
 class CreateTest {
 
-    @Inject
-    EntityManager em;
+    private static EntityManagerFactory emf = null;
+
+    private EntityManager persistenceManager() {
+        if (emf == null || !emf.isOpen()) {
+            // Lazy load der EntityManagerFactory. Siehe Kommentar oben!
+            emf = Persistence.createEntityManagerFactory("TupLink");
+        }
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        return em;
+    }
+
 
     private static long counter = 1;
 
     @Test
-    @Transactional
     void create() {
-        TestEntity orig = new TestEntity(counter++);
-        em.persist(orig);
-        em.lock(orig, LockModeType.PESSIMISTIC_WRITE);
+        final EntityManager em = persistenceManager();
+        try {
+            TestEntity orig = new TestEntity(counter++);
+            em.persist(orig);
+            em.lock(orig, LockModeType.PESSIMISTIC_WRITE);
+        } finally {
+            em.getTransaction().rollback();
+            em.close();
+        }
     }
 
     @Test
-    @Transactional
     void createLock() {
-        TestEntity orig = new TestEntity(counter++);
-        em.persist(orig);
+        final EntityManager em = persistenceManager();
+        try {
+            TestEntity orig = new TestEntity(counter++);
+            em.persist(orig);
 
-        TestEntity found = em.find(TestEntity.class, orig.id());
-        assertNotNull(found);
-        em.lock(found, LockModeType.PESSIMISTIC_WRITE);
+            TestEntity found = em.find(TestEntity.class, orig.id());
+            assertNotNull(found);
+            em.lock(found, LockModeType.PESSIMISTIC_WRITE);
+        } finally {
+            em.getTransaction().rollback();
+            em.close();
+        }
     }
 
     @Test
-    @Transactional
     void createFindLock() {
-        TestEntity orig = new TestEntity(counter++);
-        em.persist(orig);
+        final EntityManager em = persistenceManager();
+        try {
+            TestEntity orig = new TestEntity(counter++);
+            em.persist(orig);
 
-        TestEntity found = em.find(TestEntity.class, orig.id(), LockModeType.PESSIMISTIC_WRITE);
-        assertNotNull(found);
+            TestEntity found = em.find(TestEntity.class, orig.id(), LockModeType.PESSIMISTIC_WRITE);
+            assertNotNull(found);
+        } finally {
+            em.getTransaction().rollback();
+            em.close();
+        }
     }
 
     @Test
-    @Transactional
     void createLockNoVers() {
-        TestNoVersEntity orig = new TestNoVersEntity(counter++);
-        em.persist(orig);
+        final EntityManager em = persistenceManager();
+        try {
+            TestNoVersEntity orig = new TestNoVersEntity(counter++);
+            em.persist(orig);
 
-        TestNoVersEntity found = em.find(TestNoVersEntity.class, orig.id());
-        assertNotNull(found);
-        em.lock(found, LockModeType.PESSIMISTIC_WRITE);
+            TestNoVersEntity found = em.find(TestNoVersEntity.class, orig.id());
+            assertNotNull(found);
+            em.lock(found, LockModeType.PESSIMISTIC_WRITE);
+        } finally {
+            em.getTransaction().rollback();
+            em.close();
+        }
     }
 
     @Test
-    @Transactional
     void createFindLockNoVers() {
-        TestNoVersEntity orig = new TestNoVersEntity(counter++);
-        em.persist(orig);
+        final EntityManager em = persistenceManager();
+        try {
+            TestNoVersEntity orig = new TestNoVersEntity(counter++);
+            em.persist(orig);
 
-        TestNoVersEntity found = em.find(TestNoVersEntity.class, orig.id(), LockModeType.PESSIMISTIC_WRITE);
-        assertNotNull(found);
+            TestNoVersEntity found = em.find(TestNoVersEntity.class, orig.id(), LockModeType.PESSIMISTIC_WRITE);
+            assertNotNull(found);
+        } finally {
+            em.getTransaction().rollback();
+            em.close();
+        }
     }
 
 }
