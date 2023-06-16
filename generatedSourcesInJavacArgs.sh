406e2d146c6e79f989301e9e0c99637092200cc5
Daniel Mensinger <d.mensinger@tup.com>
2023-06-16 14:20:40 +0200

    More stuff

--- generatedSourcesInJavacArgs.sh
+++ generatedSourcesInJavacArgs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 # Clean cache and setup
 mvn clean compile
 
 # Modify the generated file
 touch code/target/generated-sources/annotations/bar/MyGeneratedClass.java
+touch code/src/main/java/bar/MyAnnotatedClass.java
 
 echo -e '\n\n\n\n'
 
