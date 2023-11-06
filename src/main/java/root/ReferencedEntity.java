9867d7bc0240804138d19a93229df5464a73ba0f
Daniel Mensinger <d.mensinger@tup.com>
2023-11-06 15:59:45 +0100

    Fix Constructor

--- src/main/java/root/ReferencedEntity.java
+++ src/main/java/root/ReferencedEntity.java
@@ -11,7 +11,7 @@ public class ReferencedEntity {
 
     private String status;
 
-    private ReferencedEntity() {
+    protected ReferencedEntity() {
     }
 
     public ReferencedEntity(long id, String status) {
